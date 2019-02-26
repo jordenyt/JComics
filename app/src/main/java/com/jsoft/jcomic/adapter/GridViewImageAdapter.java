cdb64c25e1062f3729a0c1ba517d0083daf0eebf
Jorden Tse <jordenyt@gmail.com>
2019-02-26 21:08:20 +0800

    Update for DownloadImageTask

--- app/src/main/java/com/jsoft/jcomic/adapter/GridViewImageAdapter.java
+++ app/src/main/java/com/jsoft/jcomic/adapter/GridViewImageAdapter.java
@@ -18,25 +18,26 @@ import android.widget.TextView;
 import com.jsoft.jcomic.GridViewActivity;
 import com.jsoft.jcomic.R;
 import com.jsoft.jcomic.helper.BookDTO;
+import com.jsoft.jcomic.helper.DownloadImageTask;
+import com.jsoft.jcomic.helper.DownloadTaskListener;
 import com.jsoft.jcomic.helper.Utils;
 
 import java.io.File;
-import java.io.InputStream;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class GridViewImageAdapter extends BaseAdapter {
     private GridViewActivity activity;
-    private int imageWidth;
+    //private int imageWidth;
     private List<BookDTO> books;
-    private Map<Integer, View> viewMap;
+    //private Map<Integer, View> viewMap;
 
     public GridViewImageAdapter(GridViewActivity activity, int imageWidth, List<BookDTO> books) {
         this.activity = activity;
-        this.imageWidth = imageWidth;
+        //this.imageWidth = imageWidth;
         this.books = books;
-        viewMap = new HashMap<Integer, View>();
+        //viewMap = new HashMap<Integer, View>();
         //Log.e("jComics", "GridViewAdapter constructor");
     }
 
@@ -64,7 +65,7 @@ public class GridViewImageAdapter extends BaseAdapter {
 
         TextView textViewItem = (TextView) convertView.findViewById(R.id.downloadBookTitle);
 
-        ImageView imageView = (ImageView) convertView.findViewById(R.id.bookImage);
+        final ImageView imageView = (ImageView) convertView.findViewById(R.id.bookImage);
 
         boolean offlineAvailable = false;
         File bookFile = new File(Utils.getBookFile(books.get(position)),"book.json");
@@ -88,7 +89,15 @@ public class GridViewImageAdapter extends BaseAdapter {
             imageView.setAlpha(1f);
         }
         if (books.get(position).getBookImg() == null) {
-            executeAsyncTask(new DownloadImageTask(imageView, books.get(position), position), books.get(position).getBookImgUrl());
+            //executeAsyncTask(new DownloadImageTask(imageView, books.get(position), position), books.get(position).getBookImgUrl());
+            final int bookPosition = position;
+            DownloadImageTask task = new DownloadImageTask(new DownloadTaskListener() {
+                public void onDownloadImagePostExecute(String imgUrl, Bitmap result) {
+                    books.get(bookPosition).setBookImg(result);
+                    imageView.setImageBitmap(result);
+                }
+            }, null);
+            task.execute(books.get(position).getBookImgUrl());
         } else {
             imageView.setImageBitmap(books.get(position).getBookImg());
         }
@@ -110,44 +119,5 @@ public class GridViewImageAdapter extends BaseAdapter {
             activity.getEpisodeList(position);
         }
 
-    }
-
-    @TargetApi(Build.VERSION_CODES.HONEYCOMB) // API 11
-    public static <T> void executeAsyncTask(AsyncTask<T, ?, ?> asyncTask, T... params) {
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB)
-            asyncTask.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, params);
-        else
-            asyncTask.execute(params);
-    }
-
-    private class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
-        ImageView bmImage;
-        BookDTO book;
-        int position;
-
-        public DownloadImageTask(ImageView bmImage, BookDTO book, int position) {
-            this.bmImage = bmImage;
-            this.book = book;
-            this.position = position;
-        }
-
-        protected Bitmap doInBackground(String... urls) {
-            String urldisplay = urls[0];
-            Bitmap mIcon11 = null;
-            try {
-                InputStream in = new java.net.URL(urldisplay).openStream();
-                mIcon11 = BitmapFactory.decodeStream(in);
-                in.close();
-            } catch (Exception e) {
-                Log.e("jComic", "GridViewImageAdapter:doInBackground " + e.getMessage());
-                e.printStackTrace();
-            }
-            return mIcon11;
-        }
-
-        protected void onPostExecute(Bitmap result) {
-            book.setBookImg(result);
-            bmImage.setImageBitmap(result);
-        }
     }
 }
