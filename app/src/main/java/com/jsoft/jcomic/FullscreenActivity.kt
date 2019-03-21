5144494a9828efd76e902613c315541d3f4e35dc
Jorden Tse <jordenyt@gmail.com>
2019-03-21 16:47:46 +0800

    Force Immersive mode for FullScreenActivity

--- app/src/main/java/com/jsoft/jcomic/FullscreenActivity.kt
+++ app/src/main/java/com/jsoft/jcomic/FullscreenActivity.kt
@@ -57,6 +57,34 @@ class FullscreenActivity : AppCompatActivity(), SeekBar.OnSeekBarChangeListener,
 
     }
 
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+        if (hasFocus) hideSystemUI()
+    }
+
+    private fun hideSystemUI() {
+        // Enables regular immersive mode.
+        // For "lean back" mode, remove SYSTEM_UI_FLAG_IMMERSIVE.
+        // Or for "sticky immersive," replace it with SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+        window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_IMMERSIVE
+                // Set the content to appear under the system bars so that the
+                // content doesn't resize when the system bars hide and show.
+                or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                // Hide the nav bar and status bar
+                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_FULLSCREEN)
+    }
+
+    // Shows the system bars by removing all the flags
+// except for the ones that make the content appear under the system bars.
+    private fun showSystemUI() {
+        window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN)
+    }
+
     override fun onEpisodeFetched(episode: EpisodeDTO) {
         setContentView(R.layout.activity_fullscreen)
         this.seekBarLayout = findViewById(R.id.seekbar_layout)
